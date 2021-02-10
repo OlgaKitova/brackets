fd312956e1bd5e17ba21c073dc4f0c5204cc53de
Olga Kitova <olga.vizgalova@bk.ru>
2021-02-10 15:46:27 +0500

    feat: realise brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let   firstBracket = bracketsConfig.map(item => item[0]),
+        secondBraket = bracketsConfig.map(item => item[1]),
+        total = [],
+        firstIndex, secondIndex;
+
+  for (let i = 0; i < str.length; i++) {
+    firstIndex = firstBracket.indexOf(str[i]);
+    secondIndex = secondBraket.indexOf(str[i]);
+
+    if (secondIndex === firstIndex) {
+      if (total.length > 0 && total[total.length - 1] === secondIndex) {
+        total.pop();
+      } else {
+        total.push(firstIndex);
+      }
+    } else if (firstIndex != -1) {
+      total.push(firstIndex);
+    } else {
+      if (total[total.length - 1] === secondIndex) {
+        total.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  
+  return total.length === 0;
 }
